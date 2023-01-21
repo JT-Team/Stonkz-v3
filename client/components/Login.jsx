1cd94eca1272c17a6758d0e42793636af80a8f5b
NicJax <nicolasljax@gmail.com>
2023-01-21 12:49:11 -0500

    mui npm installs'

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,8 +1,20 @@
 import React, { Component, useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 const Login = () => {
-    
+
+
+    return (
+        <div>
+            <div>
+                <h4>Hello World</h4>
+            </div>
+            <div>
+              <Button variant="contained">Hello World</Button>
+            </div>
+        </div>
+        );
 }
 
 export default Login;
