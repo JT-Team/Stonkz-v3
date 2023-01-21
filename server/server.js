5ac77d4c433beaf5939829a0be903051ee5d81e5
Jong Hyun David Koo <koo@Jongs-MacBook-Air.local>
2023-01-21 11:45:21 -0500

    created barebones for servers

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const router = require('./routes');
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/',(req,res) => {
+  res.send('hello world')
+})
+
+
+
+
+
+
+
+
+
+
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
