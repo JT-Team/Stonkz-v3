46e04ea43af1308b10a2ef80b2e60e7646ada4cc
Jian Lu <jason.lu1932@gmail.com>
2023-01-21 15:54:34 -0500

    able to grab stock data from backend to frontend

---
+++ client/components/subComponents/Logo.jsx
@@ -0,0 +1,13 @@
+import React, { Component, useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const Logo = () => {
+    return (
+        <div className="logo-container">
+            <img className="logo-img" src=""></img>
+            <p>Stonk<span>Z</span></p>
+        </div>
+    )
+}
+
+export default Logo;
--- server/controllers/finnhubController.js
+++ server/controllers/finnhubController.js
@@ -2,12 +2,10 @@ const finnhubController = {};
 
 finnhubController.getStockData = (req, res, next) => {
   const { ticker } = req.query;
-  console.log(ticker);
   fetch(`https://api.twelvedata.com/time_series?apikey=b0aee648ba8d47a89de46a4dbaa9d83d&interval=1min&symbol=${ticker}&type=stock&end_date=2023-01-21 17:00:00&format=JSON&start_date=2023-01-18 09:00:00`)
-  .then(res => res.json())
-  .then(res => {
-    console.log(res)
-    res.locals.stockData;
+  .then(data => data.json())
+  .then(data => {
+    res.locals.stockData = data;
     return next();
   })
   .catch(err => {
