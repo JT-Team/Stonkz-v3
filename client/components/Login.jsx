52c7a2e663daf169de90d14f38fc4ab44ebc9019
NicJax <nicolasljax@gmail.com>
2023-01-21 12:40:02 -0500

    testing

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,19 +1,8 @@
 import React, { Component, useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
-import Button from '@mui/material/Button';
 
 const Login = () => {
-
-    return (
-    <div>
-        <div>
-            <h4>Hello World</h4>
-        </div>
-        <div>
-          <Button variant="contained">Hello World</Button>
-        </div>
-    </div>
-    );
+    
 }
 
 export default Login;
