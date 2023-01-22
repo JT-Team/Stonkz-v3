78fb0624d73db712041b3d28cc7865e8730a561e
NicJax <nicolasljax@gmail.com>
2023-01-22 18:14:20 -0500

    Working on react routers and getting state

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,17 +1,91 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { Component, useState, useEffect, useReducer} from 'react';
 import { useNavigate } from 'react-router-dom';
-// import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
+import { FormControl } from '@mui/material';
+import { FormLabel } from '@mui/material';
+import { FormHelperText } from '@mui/material';
+import { InputLabel } from '@mui/material';
+import { Input } from '@mui/material';
+import { TextField} from '@mui/material';
 
-const Login = () => {
+
+
+
+
+const useIsBoxChecked = () => {
+  const [isChecked, setIsChecked] = React.useState(false);
+  return {isChecked, turnIsCheckedOn: () => setIsChecked(true)}
+}
+
+
+const Login = (props) => {
+    const [username, updateUserName] = useState('');
+    const [password, updatePassword] = useState('');
+
+
+
+
+    // const [loggedInState, updateLoggedIn] = useState(loggedInState);
+    // const currentUser = useCurrentUser(); // This doesn't actually exist 
+
+    const [loggedIn, updateLoggedIn] = useState(false);
+
+    // const [isChecked, setIsChecked] = useState(false);
+    // const turnIsCheckedOn = () => setIsChecked(true);
+
+    const {isChecked, turnIsCheckedOn} = useIsBoxChecked(); 
+
+    const loggedInCheckboxOnChange = (loggedIn) => {
+      updateLoggedIn(loggedIn)
+      //  updateLoggedIn(previousLoggedInState => ({...previousLoggedInState, loggedIn: loggedIn})); 
+
+    }
+
+
   return (
     <div className='login'>
       <div className='loginLeft'>
         <h4>IMAGE HERE</h4>
+        <FormControl>
+            
+          <InputLabel htmlFor='my-input'>Email address</InputLabel>
+          <Input id='my-input' aria-describedby='my-helper-text' />
+          <FormHelperText id='my-helper-text'>
+            This won't be shared with anyone!
+          </FormHelperText>
+          <TextField
+            id='outlined-name'
+            label='Username'
+            value={'Test Input'}
+            onChange={console.log('test1')}
+          />
+        </FormControl>
       </div>
       <div className='loginRight'>
         <div>
+          
         </div>
-        <Button variant='contained'>Hello World</Button>
+        <form action='http://localhost:3000/user/add' method='POST'>
+          <div>
+            <label for='say'>First Name:</label>
+            <input name='first_name' id='say' value='' />
+          </div>
+          <div>
+            <label for='to'>Last Name:</label>
+            <input name='last_name' id='to' value='' />
+          </div>
+          <div>
+            <button>Add a user</button>
+          </div>
+        </form>
+        <Button variant='contained' onClick={()=> {
+            console.log('hello!')
+            props.updateUserInfo(true);
+            console.log(props.userInfo)}}>Hello World</Button>
+      </div>
+      <div> 
+        <div>{`My checkbox is ${isChecked ? 'on' : 'off'}`}</div>
+        <button onClick={turnIsCheckedOn}>Turn on checkbox</button>        
       </div>
     </div>
   );
--- client/index.js
+++ client/index.js
@@ -5,6 +5,43 @@ import Home from './components/Home.jsx'
 import Login from './components/Login.jsx'
 import Settings from './components/Settings.jsx'
 import Signup from './components/Signup.jsx'
+import { Navigate, Outlet } from 'react-router-dom'
+
+
+
+//creating a private routes wrapper that will aid in redirecting to the correct page (home or login page)
+const PrivateRoutes = (props) => {
+    let authentication = props.userInfo;
+    return (
+        authentication ? <Outlet/> : <Navigate to = '/login'/>
+    )
+}
+
+//Creating a wrapper component for all routes and strict mode to hold state from top level
+const App = () => {
+
+    const [userInfo, updateUserInfo] = React.useState(undefined); 
+
+    // if (userInfo) {
+
+    // }
+
+    return (
+        <React.StrictMode>
+             <BrowserRouter>
+                <Routes>
+                    <Route element = {<PrivateRoutes userInfo = {userInfo}/>}>
+                        <Route index element={<Home />} />
+                        <Route path="/settings" element={<Settings />} />
+                    </Route>
+                    <Route path="/login" element={<Login userInfo = {userInfo} updateUserInfo = {updateUserInfo} ><div>This Is a test for Login Page</div></Login>} />
+                    <Route path="/signup" element={<Signup />} />
+                 </Routes>
+            </BrowserRouter>
+    </React.StrictMode>
+    )
+}
+
 
 //create root for react
 const rootElement = document.getElementById("root");
@@ -12,15 +49,4 @@ const root = createRoot(rootElement);
 
 //render componenets to root
 //renders different components based on route
-root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/settings" element={<Settings />} />
-            </Routes>
-        </BrowserRouter>
-    </React.StrictMode>
-)
+root.render(<App/>)
